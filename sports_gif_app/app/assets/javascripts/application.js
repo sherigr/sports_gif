1cfc13c4dbd8167feabea0371b5ea23a393c5f2b
sherigr <SGrossman00@gmail.com>
2014-12-18 16:38:54 -0500

    Load more gifs on page load, search more gifs

--- sports_gif_app/app/assets/javascripts/application.js
+++ sports_gif_app/app/assets/javascripts/application.js
@@ -15,25 +15,45 @@
 //= require turbolinks
 //= require_tree .
 
+
+
+var gifsOffset = 50;
+var searchOffset = 25;
+
+
 $(function() {
 	console.log('Up and Running');
 	// click/play individual gif
 	renderGifs()
 	$('#gifs').on('click', 'img', viewGif);
 	$('#search-button').click(searchGifs);
+	$('#moreGifs').click(moreGifs);
 	$('#search-input').keydown(function (e) {
 		if (e.keyCode === 13) {
 			searchGifs()
 		}
 	});	
 });
-$('#gifs').on('click', 'img', moreGifs);
+	
 
-function renderGifs() {
-	$.get('http://api.giphy.com/v1/gifs/search?q=sports&api_key=dc6zaTOxFJmzC').done(function(gifs) {
+
+// function renderGifs() {	
+// 	$.get('http://api.giphy.com/v1/gifs/search?q=sports&api_key=dc6zaTOxFJmzC').done(function(gifs) {
+// 		for (var i = 0; i < gifs.data.length; i++) {
+// 			var $img = $('<img />').attr('src', gifs.data[i].images.fixed_height.url); 			
+// 			$('#gifs').append($img);
+
+// 		}	
+// 	});
+// };
+
+
+function renderGifs() {	
+	$.get('http://api.giphy.com/v1/gifs/search?q=sports&api_key=dc6zaTOxFJmzC&limit=50').done(function(gifs) {
 		for (var i = 0; i < gifs.data.length; i++) {
-			var $img = $('<img />').attr('src', gifs.data[i].images.fixed_height.url); 
+			var $img = $('<img />').attr('src', gifs.data[i].images.fixed_height.url); 			
 			$('#gifs').append($img);
+
 		}	
 	});
 };
@@ -49,7 +69,7 @@ function viewGif() {
 function searchGifs() {
 	var userInput = $('#search-input').val()
 	console.log(userInput);
-	$.get('http://api.giphy.com/v1/gifs/search?q=' + userInput + '&api_key=dc6zaTOxFJmzC').done(function(gifs) {
+	$.get('http://api.giphy.com/v1/gifs/search?q=' + userInput + '&api_key=dc6zaTOxFJmzC&offset=').done(function(gifs) {
 		console.log(gifs);
 
 		$('#gifs').empty();
@@ -57,22 +77,33 @@ function searchGifs() {
 			var $img = $('<img />').attr('src', gifs.data[i].images.fixed_height.url);
 			$('#gifs').append($img);
 		}
-
 	});
+	searchOffset += 25;
+	
 };
 
 
 function moreGifs() {
-	offset += 50;
-	$.get('http://api.giphy.com/v1/gifs/search?q=sports&api_key=dc6zaTOxFJmzC').done(function(gifs) {
-		var $img=$('<img />').attr('src', gifs.data[i].images.fixed_height.url);
-		$('#gifs').append($img);
-	});
-			
+	console.log('working') // Remove this once function working right
+	$.get('http://api.giphy.com/v1/gifs/search?q=sports&api_key=dc6zaTOxFJmzC&offset=' + gifsOffset).done(function(gifs) {
+		for (var i = 0; i < gifs.data.length; i++) {
+			var $img = $('<img />').attr('src', gifs.data[i].images.fixed_height.url); 			
+			$('#gifs').append($img);
+		}	
+	});			
+	gifsOffset += 25;
 };
 		
 
+// function moreGifs() {
+// 	console.log('working') // Remove this once function working right
+// 	$.get('http://api.giphy.com/v1/gifs/search?q=sports&api_key=dc6zaTOxFJmzC&offset=' + gifsOffset).done(function(gifs) {
+// 		var $img=$('<img />').attr('src', gifs.data[i].images.fixed_height.url);
+// 		$('#gifs').append($img);
+// 	});			
 
+// 	gifsOffset += 25;
+// };
 
 
 
